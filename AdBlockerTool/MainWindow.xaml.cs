e8ab894c0521c9eb46e42ef6a79340a08a8c96c1
Guojing GJ1 Cheng <chenggj1@lenovo.com>
2024-05-16 15:56:31 +0800

    add files

---
+++ AdBlockerTool/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics;
+using WinRT.Interop;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace AdBlockerTool
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        private AppWindow m_appWindow;
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            m_appWindow = AppWindow.GetFromWindowId(myWndId);
+            if (m_appWindow != null)
+            {
+                m_appWindow.Title = "AdBlocker Tool";
+            }
+            var ScreenHeight = DisplayArea.Primary.WorkArea.Height;
+            var ScreenWidth = DisplayArea.Primary.WorkArea.Width;
+            m_appWindow.MoveAndResize(new RectInt32(ScreenWidth / 2 - 520, (int)(ScreenHeight / 2 - 300), 1140, 700));
+        }
+
+        private string GetExtensionsID(bool isEdge = true)
+        {
+            string extensionsID = "";
+            //if (this.adblock.IsChecked == true)
+            //{
+            //    extensionsID = this.Edge.IsChecked.Value ? "ndcileolkflehcjpmjnfbnaibdcgglog" : "gighmmpiobklfepjocnamgkkbiglidom";
+            //}
+            //else if (this.adblockPlus.IsChecked == true)
+            //{
+            //    extensionsID = isEdge ? "gmgoamodcdcjnbaobigkjelfplakmdhh" : "cfhdojbkjhnklbpkdaibdccddilifddb";
+            //}
+            //else
+            //{
+            //    extensionsID = this.Edge.IsChecked.Value ? "pdffkfellgipmhklpdmokmckkkfcopbh" : "bgnkhhnnamicmpeenaelnjfhikgbkllg";
+            //}
+
+            extensionsID = isEdge ? "pdffkfellgipmhklpdmokmckkkfcopbh" : "bgnkhhnnamicmpeenaelnjfhikgbkllg";
+
+            return extensionsID;
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.status.Text = "";
+            if (this.ForceInstall.IsChecked == true)
+            {
+                MyRegisterManager.AddKeyToPolicy(this.GetExtensionsID(), this.Edge.IsChecked.Value);
+            }
+            else
+            {
+                MyRegisterManager.AddKeyToNormalAddress(this.GetExtensionsID(), this.Edge.IsChecked.Value);
+            }
+
+            this.status.Text = "安装完成";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.status.Text = "";
+            if (this.ForceInstall.IsChecked == true)
+            {
+                MyRegisterManager.UninstallPolicyKey(this.GetExtensionsID(), this.Edge.IsChecked.Value);
+            }
+            else
+            {
+                MyRegisterManager.UninstallNormalAddress(this.GetExtensionsID(), this.Edge.IsChecked.Value);
+            }
+
+
+            this.status.Text = "卸载完成";
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            MyRegisterManager.AddKeyToNormalAddress(this.GetExtensionsID(true), true);
+
+            MyRegisterManager.AddKeyToNormalAddress(this.GetExtensionsID(false), false);
+            this.Switch.Content = "Disable";
+        }
+
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MyRegisterManager.UninstallNormalAddress(this.GetExtensionsID(isEdge: true), isEdge: true);
+            MyRegisterManager.UninstallNormalAddress(this.GetExtensionsID(isEdge: false), isEdge: false);
+
+            this.Switch.Content = "Enable";
+        }
+
+        private void ToggleButton1_Checked(object sender, RoutedEventArgs e)
+        {
+            MyRegisterManager.AddKeyToPolicy(this.GetExtensionsID(true), true);
+
+            MyRegisterManager.AddKeyToPolicy(this.GetExtensionsID(false), false);
+            this.Switch2.Content = "Disable";
+        }
+
+        private void ToggleButton1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MyRegisterManager.UninstallPolicyKey(this.GetExtensionsID(isEdge: true), isEdge: true);
+            MyRegisterManager.UninstallPolicyKey(this.GetExtensionsID(isEdge: false), isEdge: false);
+
+            this.Switch2.Content = "Enable";
+        }
+    }
+}
---
+++ AdBlockerTool/MyRegisterManager.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdBlockerTool
+{
+    internal class MyRegisterManager
+    {
+        ////Edge
+        private static string _edge_key = "SOFTWARE\\WOW6432Node\\Microsoft\\Edge\\Extensions";
+        private static string _edge_update_url = "https://edge.microsoft.com/extensionwebstorebase/v1/crx";
+        private static string _edge_policy_key = "SOFTWARE\\Policies\\Microsoft\\Edge\\ExtensionInstallForcelist";
+
+        /// <summary>
+        /// chrome
+        /// </summary>
+        private static string _chrome_key = "SOFTWARE\\WOW6432Node\\Google\\Chrome\\Extensions";
+        private static string _chrome_update_url = "https://clients2.google.com/service/update2/crx";
+        private static string _chrome_policy_key = "SOFTWARE\\Policies\\Google\\Chrome\\ExtensionInstallForcelist";
+
+
+        public static void AddKeyToPolicy(string extensionsId, bool isEdge)
+        {
+            string update_url = isEdge ? _edge_update_url : _chrome_update_url;
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(isEdge ? _edge_policy_key : _chrome_policy_key, true);
+            if (registryKey != null)
+            {
+                var names = registryKey.GetValueNames();
+                if (names.Length > 0)
+                {
+                    var orderedNames = names.OrderDescending();
+                    int lastIndex = -1;
+                    if (Int32.TryParse(orderedNames.Last(), out lastIndex))
+                    {
+                        registryKey.SetValue((lastIndex + 1).ToString(), $"{extensionsId};{update_url}");
+                    }
+                }
+
+                registryKey.Close();
+            }
+        }
+
+        public static void AddKeyToNormalAddress(string extensionsId, bool isEdge)
+        {
+            string update_url = isEdge ? _edge_update_url : _chrome_update_url;
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(isEdge ? _edge_key : _chrome_key, true);
+            if (registryKey != null)
+            {
+                var extensionKey = registryKey.CreateSubKey(extensionsId);
+                extensionKey.SetValue("update_url", update_url);
+
+                registryKey.Close();
+            }
+        }
+
+        public static void UninstallPolicyKey(string extensionsId, bool isEdge)
+        {
+            string update_url = isEdge ? _edge_update_url : _chrome_update_url;
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(isEdge ? _edge_policy_key : _chrome_policy_key, true);
+            if (registryKey != null)
+            {
+                var names = registryKey.GetValueNames();
+                foreach (var name in names)
+                {
+                    string value = registryKey.GetValue(name).ToString();
+                    if (value != null)
+                    {
+                        if (value.StartsWith(extensionsId))
+                        {
+                            registryKey.DeleteValue(name);
+                        }
+                    }
+                }
+
+                registryKey.Close();
+            }
+        }
+
+        public static void UninstallNormalAddress(string extensionsId, bool isEdge)
+        {
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey(isEdge ? _edge_key : _chrome_key, true);
+            if (registryKey != null)
+            {
+                var names = registryKey.GetSubKeyNames();
+                if(names.Length > 0 && names.Any(key => key.Trim().Equals(extensionsId.Trim(), StringComparison.OrdinalIgnoreCase)))
+                {
+                    registryKey.DeleteSubKey(extensionsId);
+                }
+
+                registryKey.Close();
+            }
+        }
+    }
+}
